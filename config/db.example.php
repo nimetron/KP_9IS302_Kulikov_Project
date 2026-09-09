2bc8e26e757e681610ac9c607dd36197b912d033
nimetron <vitec530@gmail.com>
2026-09-09 23:39:27 +0700

    fix: sftp sync active

--- config/db.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config/db.example.php — образец конфигурации
-return [
-    'host'     => 'localhost',
-    'dbname'   => 'your_db_name',
-    'user'     => 'your_db_user',
-    'password' => 'your_db_password',
-    'charset'  => 'utf8mb4'
-];
